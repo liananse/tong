06ba0b030a7a55cd54b9234be201b15975fa3b01
liananse <liananse@163.com>
2015-01-05 15:03:12 +0800

    Commit
    聊天

--- src/com/mobilepower/tong/ui/activity/ChatListActivity.java
+++ src/com/mobilepower/tong/ui/activity/ChatListActivity.java
@@ -1,10 +1,21 @@
 package com.mobilepower.tong.ui.activity;
 
-import com.mobilepower.tong.R;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Hashtable;
+import java.util.List;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+
+import com.easemob.chat.EMChatManager;
+import com.easemob.chat.EMConversation;
+import com.easemob.chat.EMMessage;
+import com.mobilepower.tong.R;
+import com.mobilepower.tong.ui.adapter.ChatAllHistoryAdapter;
+import com.mobilepower.tong.ui.view.XListView;
 
 public class ChatListActivity extends BaseActivity implements OnClickListener{
 
@@ -14,6 +25,8 @@ public class ChatListActivity extends BaseActivity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.chat_list_activity);
 		initActionBar();
+		
+		initData();
 	}
 	
 	private View mBackBtn;
@@ -26,6 +39,70 @@ public class ChatListActivity extends BaseActivity implements OnClickListener{
 
 		mBackBtn.setOnClickListener(this);
 	}
+	
+	private List<EMConversation> conversationList;
+	
+	private XListView mList;
+	private ChatAllHistoryAdapter adapter;
+	private void initData() {
+		mList = (XListView) findViewById(R.id.chat_list);
+		mList.setPullLoadEnable(false);
+		mList.setPullRefreshEnable(false);
+		
+		conversationList = loadConversationsWithRecentChat();
+		adapter = new ChatAllHistoryAdapter(this, 1, conversationList);
+		// 设置adapter
+		mList.setAdapter(adapter);
+		for (int i = 0; i < conversationList.size(); i++) {
+			System.out.println(conversationList.get(i).getUserName());
+		}
+	}
+	
+	/**
+	 * 获取所有会话
+	 * 
+	 * @param context
+	 * @return
+	 */
+	private List<EMConversation> loadConversationsWithRecentChat() {
+		// 获取所有会话，包括陌生人
+		Hashtable<String, EMConversation> conversations = EMChatManager.getInstance().getAllConversations();
+		List<EMConversation> list = new ArrayList<EMConversation>();
+		// 过滤掉messages seize为0的conversation
+		for (EMConversation conversation : conversations.values()) {
+			if (conversation.getAllMessages().size() != 0)
+				list.add(conversation);
+		}
+		// 排序
+		sortConversationByLastChatTime(list);
+		return list;
+	}
+
+	/**
+	 * 根据最后一条消息的时间排序
+	 * 
+	 * @param usernames
+	 */
+	private void sortConversationByLastChatTime(List<EMConversation> conversationList) {
+		Collections.sort(conversationList, new Comparator<EMConversation>() {
+			@Override
+			public int compare(final EMConversation con1, final EMConversation con2) {
+
+				EMMessage con2LastMessage = con2.getLastMessage();
+				EMMessage con1LastMessage = con1.getLastMessage();
+				if (con2LastMessage.getMsgTime() == con1LastMessage.getMsgTime()) {
+					return 0;
+				} else if (con2LastMessage.getMsgTime() > con1LastMessage.getMsgTime()) {
+					return 1;
+				} else {
+					return -1;
+				}
+			}
+
+		});
+	}
+	
+	
 
 	@Override
 	protected void onResume() {
